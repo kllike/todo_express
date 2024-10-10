ddb168db7f3b3ae2d7a63280e0c350bc0eb22bef
Kullike Annuk <kullike.annuk@voco.ee>
2024-10-10 13:56:15 +0300

    Read data from file

--- index.js
+++ index.js
@@ -3,13 +3,22 @@ const { default: test } = require('node:test')
 
 const app = express()
 const path = require('path')
+const fs = require('node:fs');
+
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 app. get ('/', (req, res) => {
-    res.render('index')
+    fs.readFile('./tasks', 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        const tasks = data.split("\n") 
+        res.render('index', {tasks: tasks} )
+      });
 } )
 app. listen(3001, () => {
     console.log('Server is started http://localhost:3001')
-} )
+} )                     
